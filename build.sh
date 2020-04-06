faf0d0011ef23601ea6784e0fa27fca922e93c39
Onur Can Cakmak <occ@occ.me>
2020-04-06 01:52:33 -0400

    Move build logic to build.sh

--- build.sh
+++ build.sh
@@ -5,3 +5,7 @@ MARLIN_VERSION="2.0.5.3"
 wget https://github.com/MarlinFirmware/Marlin/archive/${MARLIN_VERSION}.zip
 unzip ${MARLIN_VERSION}.zip
 mv Marlin-${MARLIN_VERSION} Marlin
+
+arduino --pref "boardsmanager.additional.urls=https://raw.githubusercontent.com/Lauszus/Sanguino/master/package_lauszus_sanguino_index.json,https://adafruit.github.io/arduino-board-index/package_adafruit_index.json,http://arduino.esp8266.com/stable/package_esp8266com_index.json,https://dl.espressif.com/dl/package_esp32_index.json" --save-prefs
+arduino --install-boards Sanguino:avr
+arduino --verify --board Sanguino:avr:sanguino:cpu=atmega1284p Marlin/Marlin/Marlin.ino
